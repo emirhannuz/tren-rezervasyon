c8b678fd64ae4a98ceabfafdcfcfd6fb3a5ad477
Emirhan <emirhannuz@gmail.com>
2021-08-25 14:53:47 +0300

    init

---
+++ src/test/java/ada/trenRezervasyon/TrenRezervasyonApplicationTests.java
@@ -0,0 +1,13 @@
+package ada.trenRezervasyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrenRezervasyonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
